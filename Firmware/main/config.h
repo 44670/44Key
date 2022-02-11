69dc839ea876b9633d40a6ac836d412ebd05fca5
44670 <44670@users.noreply.github.com>
2022-02-11 20:36:33 +0800

    set 44key board by default

--- Firmware/main/config.h
+++ Firmware/main/config.h
@@ -1,11 +1,20 @@
 
 #pragma once
 
-#define BOARD_OPENLUAT_ESP32C3
+#define BOARD_44KEY
+//#define BOARD_OPENLUAT_ESP32C3
+//#define BOARD_CUSTOM
 
-#ifdef BOARD_OPENLUAT_ESP32C3
-#define PIN_LED (12)
+#if defined(BOARD_44KEY)
+#define PIN_LED (2)
+#define LED_ACTIVE (0)
 #define PIN_BTN_CONFIRM (9)
+#elif defined(BOARD_OPENLUAT_ESP32C3)
+#define PIN_LED (12)
 #define LED_ACTIVE (1)
+#define PIN_BTN_CONFIRM (9)
+#elif defined(BOARD_CUSTOM)
+// TODO: Define your custom board here
 #else
+#error "Please define your board!"
 #endif
